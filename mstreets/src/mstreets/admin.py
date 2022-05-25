3cc3735fc25f1499f8bcf8d123f034197de3b6f3
aroiginfraplan <59873981+aroiginfraplan@users.noreply.github.com>
2022-05-25 12:41:34 +0200

    Admin

--- mstreets/src/mstreets/admin.py
+++ mstreets/src/mstreets/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from mstreets.models import PC, Campaign, Config, Metadata, Poi_Locations, Zone
+
+
+@admin.register(Config)
+class ConfigAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Metadata)
+class MetadataAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Poi_Locations)
+class Poi_LocationsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PC)
+class PCAdmin(admin.ModelAdmin):
+    pass
